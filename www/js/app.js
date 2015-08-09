a7aa232f707758c8caf7f896cb2e9ede6a43faa6
Denzel Wamburu <denzjoseph@gmail.com>
2015-08-09 14:59:02 +0300

    Removed unused codes and files, added mdinput

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('dekutapp', ['dekutapp.account', 'dekutapp.dev', 'dekutapp.home', 'dekutapp.login', 'dekutapp.register', 'dekutapp.tweet', 'ionic', 'lbServices', 'bd.timedistance', 'ngCordova', 'ionic-material', 'ngTable', 'dekutapp.controller', 'rssappControllers', 'rssappServices'])
+angular.module('dekutapp', ['dekutapp.account', 'dekutapp.dev', 'dekutapp.home', 'dekutapp.login', 'dekutapp.register', 'dekutapp.tweet', 'ionic', 'lbServices', 'bd.timedistance', 'ngCordova', 'ionic-material', 'ionMdInput', 'dekutapp.controller', 'rssappControllers', 'rssappServices'])
 
 /*.run(function ($ionicPlatform) {
  $ionicPlatform.ready(function () {
