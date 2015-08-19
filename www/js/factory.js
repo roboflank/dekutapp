d6ae3ab4f56f5cb045be0afa1dd0585d38982119
Denzel Wamburu <denzjoseph@gmail.com>
2015-08-19 20:14:03 +0300

    Fixed ContivityMonitor factory

--- www/js/factory.js
+++ www/js/factory.js
@@ -9,4 +9,48 @@ angular.module('dekutapp.factory', ['ngResource', 'dekutapp.controller'])
 //Factory for Notices
 .factory('Notice', function ($resource) {
         return $resource('http://localhost:3000/api/notices/:noticeId');
-    });
+    })
+
+//Network Info factory
+.factory('ConnectivityMonitor', function($rootScope, $cordovaNetwork){
+
+  return {
+    isOnline: function(){
+      if(ionic.Platform.isWebView()){
+        return $cordovaNetwork.isOnline();
+      } else {
+        return navigator.onLine;
+      }
+    },
+    ifOffline: function(){
+      if(ionic.Platform.isWebView()){
+        return !$cordovaNetwork.isOnline();
+      } else {
+        return !navigator.onLine;
+      }
+    },
+    startWatching: function(){
+        if(ionic.Platform.isWebView()){
+
+          $rootScope.$on('$cordovaNetwork:online', function(event, networkState){
+            console.log("Connection On");
+          });
+
+          $rootScope.$on('$cordovaNetwork:offline', function(event, networkState){
+            console.log("Please Enable Your Internet Connectivity then Proceed");
+          });
+
+        }
+        else {
+
+          window.addEventListener("online", function(e) {
+            console.log("Connection On");
+          }, false);
+
+          window.addEventListener("offline", function(e) {
+            console.log("Please Enable Your Internet Connectivity then Proceed");
+          }, false);
+        }
+    }
+  }
+});
