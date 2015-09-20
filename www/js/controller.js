fa706e16f3b3891eef1da2f7b39349f290dc46d5
Denzel Wamburu <denzjoseph@gmail.com>
2015-09-20 22:07:15 +0300

    ImportedImported code for firebase sync with modal, added controller :happy:

--- www/js/controller.js
+++ www/js/controller.js
@@ -29,6 +29,120 @@ angular.module('dekutapp.controller', ['ionic', 'ngCordova', 'ngResource', 'deku
     })
 
     //Elibrary Controller
+    /* Firebase sync. Monopoly is good */
+    .controller("PastPapersCtrl", function($scope, PastPapers) {
+            $scope.pastpapers = PastPapers;
+            $scope.addPastPaper = function() {
+                var name = prompt("Enter Your Full Name");
+                if (name) {
+                    $scope.pastpapers.$add({
+                        "name": name
+                    });
+                }
+            };
+        })
+
+        // another code-wrapper
+
+        .controller('myListCtrl', function($rootScope, $scope, $window, $ionicModal, $firebase) {
+            $rootScope.show("Please wait... Processing");
+            $scope.list = [];
+            var bucketListRef = new Firebase($rootScope.baseUrl + escapeEmailAddress($rootScope.userEmail));
+            bucketListRef.on('value', function(snapshot) {
+                var data = snapshot.val();
+                $scope.list = [];
+                for (var key in data) {
+                    if (data.hasOwnProperty(key)) {
+                        if (data[key].isCompleted == false) {
+                            data[key].key = key;
+                            $scope.list.push(data[key]);
+                        }
+                    }
+                }
+
+                if ($scope.list.length == 0) {
+                    $scope.noData = true;
+                } else {
+                    $scope.noData = false;
+                }
+                $rootScope.hide();
+            });
+
+
+            $ionicModal.fromTemplateUrl('templates/newItem.html', function(modal) {
+                $scope.newTemplate = modal;
+            });
+
+            $scope.newTask = function() {
+                $scope.newTemplate.show();
+            };
+
+            $scope.markCompleted = function(key) {
+                $rootScope.show("Please wait... Updating List");
+                var itemRef = new Firebase($rootScope.baseUrl + escapeEmailAddress($rootScope.userEmail) + '/' + key);
+                itemRef.update({
+                    isCompleted: true
+                }, function(error) {
+                    if (error) {
+                        $rootScope.hide();
+                        $rootScope.notify('Oops! something went wrong. Try again later');
+                    } else {
+                        $rootScope.hide();
+                        $rootScope.notify('Successfully updated');
+                    }
+                });
+            };
+
+            $scope.deleteItem = function(key) {
+                $rootScope.show("Please wait... Deleting from List");
+                var itemRef = new Firebase($rootScope.baseUrl + escapeEmailAddress($rootScope.userEmail));
+                bucketListRef.child(key).remove(function(error) {
+                    if (error) {
+                        $rootScope.hide();
+                        $rootScope.notify('Oops! something went wrong. Try again later');
+                    } else {
+                        $rootScope.hide();
+                        $rootScope.notify('Successfully deleted');
+                    }
+                });
+            };
+        })
+
+        .controller('newCtrl', function($rootScope, $scope, $window, $firebase) {
+            $scope.data = {
+                item: "",
+                quantity: ""
+            };
+
+            $scope.close = function() {
+                $scope.modal.hide();
+            };
+
+            $scope.createNew = function() {
+                var item = this.data.item;
+                var quantity = this.data.quantity;
+                if (!item) return;
+                $scope.modal.hide();
+                $rootScope.show();
+
+                $rootScope.show("Please wait... Creating new");
+
+                var form = {
+                    item: item,
+                    quantity: quantity,
+                    isCompleted: false,
+                    created: Date.now(),
+                    updated: Date.now()
+                };
+
+                var bucketListRef = new Firebase($rootScope.baseUrl + escapeEmailAddress($rootScope.userEmail));
+                $firebase(bucketListRef).$add(form);
+                $rootScope.hide();
+
+            };
+        })
+
+
     /* User sends text to the libary number from the app
     * Use this incase the online one is ineffective
     */
