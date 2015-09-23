fe661609e336a21fd7e7f374b1e5470ad9ce070a
Denzel Wamburu <denzjoseph@gmail.com>
2015-09-23 16:21:13 +0300

    Feedback factory created

--- www/js/services.js
+++ www/js/services.js
@@ -45,7 +45,55 @@ angular.module('dekutapp.services',[])
             });
         }
     }
-}]).value('PARSE_CREDENTIALS',{
+}])
+.factory('FeedBack',['$http','PARSE_CREDENTIALS',function($http,PARSE_CREDENTIALS){
+return {
+    getAll:function(){
+        return $http.get('https://api.parse.com/1/classes/feedback',{
+            headers:{
+                'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
+                'X-Parse-REST-API-Key':PARSE_CREDENTIALS.REST_API_KEY,
+            }
+        });
+    },
+    get:function(id){
+        return $http.get('https://api.parse.com/1/classes/feedback/'+id,{
+            headers:{
+                'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
+                'X-Parse-REST-API-Key':PARSE_CREDENTIALS.REST_API_KEY,
+            }
+        });
+    },
+    create:function(data){
+        return $http.post('https://api.parse.com/1/classes/feedback',data,{
+            headers:{
+                'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
+                'X-Parse-REST-API-Key':PARSE_CREDENTIALS.REST_API_KEY,
+                'Content-Type':'application/json'
+            }
+        });
+    },
+    edit:function(id,data){
+        return $http.put('https://api.parse.com/1/classes/feedback/'+id,data,{
+            headers:{
+                'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
+                'X-Parse-REST-API-Key':PARSE_CREDENTIALS.REST_API_KEY,
+                'Content-Type':'application/json'
+            }
+        });
+    },
+    delete:function(id){
+        return $http.delete('https://api.parse.com/1/classes/feedback/'+id,{
+            headers:{
+                'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
+                'X-Parse-REST-API-Key':PARSE_CREDENTIALS.REST_API_KEY,
+                'Content-Type':'application/json'
+            }
+        });
+    }
+}
+}])
+.value('PARSE_CREDENTIALS',{
     APP_ID: 'CaB2kjiHJoaqEG1LSeEPYJtx2Geuend5HaVZ34jU',
     REST_API_KEY:'IO1sjED77XZBwHdzRRHupTqNPAP5uXtNLFUjoTzs'
 });
